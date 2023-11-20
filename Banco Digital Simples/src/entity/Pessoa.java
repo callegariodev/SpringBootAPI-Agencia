212b4c6c5cb41a18d9b1a048c8f3d237205d2701
Caio Callegario <117415886+callegariodev@users.noreply.github.com>
2023-11-20 14:06:13 -0300

    Commit Inicial
    Commit inicial com as funcionalidades de criar a listar pessoas, usuários e contas.

---
+++ Banco Digital Simples/src/entity/Pessoa.java
@@ -0,0 +1,31 @@
+package entity;
+
+public class Pessoa {
+    private String nome;
+    private String documento;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
---
+++ Banco Digital Simples/src/entity/Usuario.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Usuario {
+    private String nomeAcesso;
+    private String senha;
+
+    public String getNomeAcesso() {
+        return nomeAcesso;
+    }
+
+    public void setNomeAcesso(String nomeAcesso) {
+        this.nomeAcesso = nomeAcesso;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
